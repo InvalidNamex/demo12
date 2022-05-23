0d29fd5facae8521b9e5e2ded385381e31a19bf1
drrio <dr.riosx@gmail.com>
2022-05-23 18:39:08 +0200

    bottom Navbar

--- lib/views/landing_screen.dart
+++ lib/views/landing_screen.dart
@@ -14,68 +14,83 @@ class LandingScreen extends StatelessWidget {
   final TextStyle selectedLabelStyle = const TextStyle(
       color: Color(0xFF235A6C), fontWeight: FontWeight.bold, fontSize: 12);
 
+  const LandingScreen({Key? key}) : super(key: key);
+
   buildBottomNavigationMenu(context, landingPageController) {
     return Obx(() => MediaQuery(
         data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
         child: Container(
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(25)),
-          height: 74,
-          child: BottomNavigationBar(
-            showUnselectedLabels: true,
-            showSelectedLabels: true,
-            onTap: landingPageController.changeTabIndex,
-            currentIndex: landingPageController.tabIndex.value,
-            backgroundColor: secondaryLightColor,
-            unselectedItemColor: const Color(0xFF5398B1),
-            selectedItemColor: const Color(0xFF235A6C),
-            unselectedLabelStyle: unselectedLabelStyle,
-            selectedLabelStyle: selectedLabelStyle,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                  size: 40.0,
-                  color: Color(0xFF305F71),
-                ),
-                label: 'الرئيسية',
-                backgroundColor: secondaryLightColor,
+            height: 74,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
               ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.menu_book,
-                  size: 40.0,
-                  color: Color(0xFF305F71),
-                ),
-                label: 'المكتبة',
+              child: BottomNavigationBar(
+                showUnselectedLabels: true,
+                showSelectedLabels: true,
+                onTap: landingPageController.changeTabIndex,
+                currentIndex: landingPageController.tabIndex.value,
                 backgroundColor: secondaryLightColor,
+                unselectedItemColor: const Color(0xFF5398B1),
+                selectedItemColor: const Color(0xFF235A6C),
+                unselectedLabelStyle: unselectedLabelStyle,
+                selectedLabelStyle: selectedLabelStyle,
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.home,
+                      size: 40.0,
+                      color: Color(0xFF305F71),
+                    ),
+                    label: 'الرئيسية',
+                    backgroundColor: secondaryLightColor,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.menu_book,
+                      size: 40.0,
+                      color: Color(0xFF305F71),
+                    ),
+                    label: 'المكتبة',
+                    backgroundColor: secondaryLightColor,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.bookmark,
+                      size: 40.0,
+                      color: Color(0xFF305F71),
+                    ),
+                    label: 'المفضلة',
+                    backgroundColor: secondaryLightColor,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.settings,
+                      size: 40.0,
+                      color: Color(0xFF305F71),
+                    ),
+                    label: 'الإعدادات',
+                    backgroundColor: secondaryLightColor,
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.bookmark,
-                  size: 40.0,
-                  color: Color(0xFF305F71),
-                ),
-                label: 'المفضلة',
-                backgroundColor: secondaryLightColor,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.settings,
-                  size: 40.0,
-                  color: Color(0xFF305F71),
-                ),
-                label: 'الإعدادات',
-                backgroundColor: secondaryLightColor,
-              ),
-            ],
-          ),
-        )));
+            ))));
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+      resizeToAvoidBottomInset: false,
       bottomNavigationBar:
           buildBottomNavigationMenu(context, landingController),
       body: Obx(() => IndexedStack(
